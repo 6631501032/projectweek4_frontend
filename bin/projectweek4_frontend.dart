908eff42fc44d79bd07c59f59cab2f9606dbd8dd
6631501032 <6631501032@lamduan.mfu.ac.th>
2025-08-23 20:40:50 +0700

    The text is complete, but it's still in progress.

--- bin/projectweek4_frontend.dart
+++ bin/projectweek4_frontend.dart
@@ -2,11 +2,13 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-void main()async {
-// ----------------- login -----------------
-    int? userId = await login();
-// ----------------- Choose Choice -----------------
-
+void main() async {
+  // ----------------- login -----------------
+  int? userId = await login();
+  // ----------------- Choose Choice -----------------
+  if (userId != null) {
+    await menu(userId);
+  }
 }
 
 //========== Future login ==========
@@ -17,14 +19,11 @@ Future<int?> login() async {
   stdout.write("Password: ");
   String? password = stdin.readLineSync()?.trim();
 
-
   if (username == null || password == null) return null;
-
 
   final body = {"username": username, "password": password};
   final url = Uri.parse('http://localhost:3000/login');
   final response = await http.post(url, body: body);
-
 
   if (response.statusCode == 200) {
     final data = jsonDecode(response.body);
@@ -34,4 +33,15 @@ Future<int?> login() async {
     print(response.body);
     return null;
   }
+}
+
+//========== Future Menu ==========
+Future<void> menu(int userId) async {
+    print("===================== Expenses Tracking App =====================");
+    print("1. All expenses");
+    print("2. Today's expense");
+    print("3. Search expense");
+    print("4. Add expense");
+    print("5. Delete expense");
+    print("6. Exit");
 }
