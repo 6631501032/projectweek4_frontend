7b63fcca09de7c74957ac7f3c5b42534e47f1a8e
6631501032 <6631501032@lamduan.mfu.ac.th>
2025-08-23 20:18:30 +0700

    initial Commit FrontEnd

---
+++ test/projectweek4_frontend_test.dart
@@ -0,0 +1,8 @@
+import 'package:projectweek4_frontend/projectweek4_frontend.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
