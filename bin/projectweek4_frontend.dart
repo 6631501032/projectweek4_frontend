dfe84344d89b3c9319d2ebf69e728cdbb4e67d76
6631501032 <6631501032@lamduan.mfu.ac.th>
2025-08-23 20:25:27 +0700

    The login feature has been completed.

--- bin/projectweek4_frontend.dart
+++ bin/projectweek4_frontend.dart
@@ -4,7 +4,34 @@ import 'dart:convert';
 
 void main()async {
 // ----------------- login -----------------
-
+    int? userId = await login();
 // ----------------- Choose Choice -----------------
 
+}
+
+//========== Future login ==========
+Future<int?> login() async {
+  print("===== Login =====");
+  stdout.write("Username: ");
+  String? username = stdin.readLineSync()?.trim();
+  stdout.write("Password: ");
+  String? password = stdin.readLineSync()?.trim();
+
+
+  if (username == null || password == null) return null;
+
+
+  final body = {"username": username, "password": password};
+  final url = Uri.parse('http://localhost:3000/login');
+  final response = await http.post(url, body: body);
+
+
+  if (response.statusCode == 200) {
+    final data = jsonDecode(response.body);
+    print("Welcome ${data['username']}!");
+    return data['userId'];
+  } else {
+    print(response.body);
+    return null;
+  }
 }
