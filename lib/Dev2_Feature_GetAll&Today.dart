b64eba90961ac93ef1fb23feb2c20672c607dc71
Note <6631501014@lamduan.mfu.ac.th>
2025-08-26 01:01:08 +0700

    Get today Expenses

--- lib/Dev2_Feature_GetAll&Today.dart
+++ lib/Dev2_Feature_GetAll&Today.dart
@@ -23,3 +23,24 @@ Future<void> getAllExpenses(int? userId) async {
     print("${e['id']}. ${e['item']} : ${e['paid']}฿ : ${e['date']}");
   }
 }
+//========== Future Get Today's Expenses ==========
+Future<void> getTodayExpenses(int? userId) async {
+  final uri = Uri.parse('http://localhost:3000/expenses/today/$userId');
+  final response = await http.get(uri);
+
+  if (response.statusCode != 200) {
+    print("Failed to retrieve today's expenses!");
+    return;
+  }
+
+  final expenses = jsonDecode(response.body) as List;
+  if (expenses.isEmpty) {
+    print("No expenses for today.");
+    return;
+  }
+
+  print("===== Today's Expenses =====");
+  for (var e in expenses) {
+    print("${e['id']}. ${e['item']} : ${e['paid']}฿ : ${e['date']}");
+  }
+}
